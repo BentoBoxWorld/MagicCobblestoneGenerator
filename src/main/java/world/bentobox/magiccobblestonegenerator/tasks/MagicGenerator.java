d0734c80378e3779d86e2b934848239fbfdd1ff0
BONNe <bonne@bonne.id.lv>
2020-06-28 19:10:33 +0300

    Rework MagicGenerator class. Use new methods and abilities.

--- src/main/java/world/bentobox/magiccobblestonegenerator/tasks/MagicGenerator.java
+++ src/main/java/world/bentobox/magiccobblestonegenerator/tasks/MagicGenerator.java
@@ -4,11 +4,13 @@ package world.bentobox.magiccobblestonegenerator.tasks;
 import java.util.Random;
 import java.util.TreeMap;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+import org.eclipse.jdt.annotation.Nullable;
 
 import world.bentobox.magiccobblestonegenerator.StoneGeneratorAddon;
+import world.bentobox.magiccobblestonegenerator.database.objects.GeneratorTierObject;
 
 
 /**
@@ -33,32 +35,11 @@ public class MagicGenerator
      */
     public boolean isCobblestoneReplacementGenerated(Block block)
     {
-        TreeMap<Double, Material> chanceMap =
-            (TreeMap<Double, Material>) this.addon.getManager().getMaterialChanceMap(
-                this.addon.getManager().getIslandLevel(block.getLocation()), block.getWorld());
+        GeneratorTierObject generatorTier = this.addon.getManager().getGeneratorTier(
+            block.getLocation(),
+            GeneratorTierObject.GeneratorType.COBBLESTONE);
 
-        if (chanceMap.isEmpty())
-        {
-            return false;
-        }
-
-        Material newMaterial;
-
-        if (chanceMap.size() == 1)
-        {
-            // no needs to calculate. It is our material.
-            newMaterial = chanceMap.get(chanceMap.firstKey());
-        }
-        else
-        {
-            double rand = this.random.nextDouble() * chanceMap.lastKey();
-            newMaterial = chanceMap.ceilingEntry(rand).getValue();
-        }
-
-        // ask config if physics should be used
-        block.setType(newMaterial, this.addon.getSettings().usePhysics());
-
-        return true;
+        return this.processBlockReplacement(block, generatorTier);
     }
 
 
@@ -69,32 +50,11 @@ public class MagicGenerator
      */
     public boolean isStoneReplacementGenerated(Block block)
     {
-        TreeMap<Double, Material> chanceMap =
-            (TreeMap<Double, Material>) this.addon.getManager().getMaterialChanceMap(
-                this.addon.getManager().getIslandLevel(block.getLocation()), block.getWorld());
+        GeneratorTierObject generatorTier = this.addon.getManager().getGeneratorTier(
+            block.getLocation(),
+            GeneratorTierObject.GeneratorType.STONE);
 
-        if (chanceMap.isEmpty())
-        {
-            return false;
-        }
-
-        Material newMaterial;
-
-        if (chanceMap.size() == 1)
-        {
-            // no needs to calculate. It is our material.
-            newMaterial = chanceMap.get(chanceMap.firstKey());
-        }
-        else
-        {
-            double rand = this.random.nextDouble() * chanceMap.lastKey();
-            newMaterial = chanceMap.ceilingEntry(rand).getValue();
-        }
-
-        // ask config if physics should be used
-        block.setType(newMaterial, this.addon.getSettings().usePhysics());
-
-        return true;
+        return this.processBlockReplacement(block, generatorTier);
     }
 
 
@@ -105,32 +65,94 @@ public class MagicGenerator
      */
     public boolean isBasaltReplacementGenerated(Block block)
     {
-        TreeMap<Double, Material> chanceMap =
-            (TreeMap<Double, Material>) this.addon.getManager().getMaterialChanceMap(
-                this.addon.getManager().getIslandLevel(block.getLocation()), block.getWorld());
+        GeneratorTierObject generatorTier = this.addon.getManager().getGeneratorTier(
+            block.getLocation(),
+            GeneratorTierObject.GeneratorType.BASALT);
 
-        if (chanceMap.isEmpty())
+        return this.processBlockReplacement(block, generatorTier);
+    }
+
+
+    /**
+     * This method tries to replace block from chance map and returns if it was successful.
+     * @param block Block that need to be replaced.
+     * @param generatorTier Object that contains all possible chances.
+     * @return {@code true} if it was successful, {@code false} otherwise.
+     */
+    private boolean processBlockReplacement(Block block, @Nullable GeneratorTierObject generatorTier)
+    {
+        if (generatorTier == null)
         {
+            // Check if generator exists.
             return false;
         }
 
-        Material newMaterial;
+        TreeMap<Double, Material> chanceMap = (TreeMap<Double, Material>) generatorTier.getBlockChanceMap();
+
+        if (chanceMap.isEmpty())
+        {
+            // Check if any block has a chance to spawn
+            return false;
+        }
+
+        Material newMaterial = this.getMaterialFromMap(chanceMap);
+
+        if (newMaterial == null)
+        {
+            // Check if a material was found
+            return false;
+        }
+
+        // ask config if physics should be used
+        block.setType(newMaterial, this.addon.getSettings().isUsePhysics());
+
+        if (generatorTier.getTreasureChance() > 0 && !generatorTier.getTreasureChanceMap().isEmpty())
+        {
+            // Random check on getting treasure.
+            if (this.random.nextDouble() <= generatorTier.getTreasureChance())
+            {
+                // Use the same variables for treasures.
+                chanceMap = (TreeMap<Double, Material>) generatorTier.getTreasureChanceMap();
+                newMaterial = this.getMaterialFromMap(chanceMap);
+
+                // Double check, in general it should always be a material.
+                if (newMaterial != null)
+                {
+                    // drop item naturally in the location of the block
+                    block.getWorld().dropItemNaturally(block.getLocation(),
+                        new ItemStack(newMaterial, this.random.nextInt(generatorTier.getMaxTreasureAmount() - 1) + 1));
+
+                    // TODO: Add effects like sound and particle :)
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * This method returns a random material from given tree map.
+     * @param chanceMap Map that contains all objects with their chance to drop.
+     * @return Material from map or null.
+     */
+    private @Nullable Material getMaterialFromMap(TreeMap<Double, Material> chanceMap)
+    {
+        if (chanceMap.isEmpty())
+        {
+            return null;
+        }
 
         if (chanceMap.size() == 1)
         {
             // no needs to calculate. It is our material.
-            newMaterial = chanceMap.get(chanceMap.firstKey());
+            return chanceMap.get(chanceMap.firstKey());
         }
         else
         {
             double rand = this.random.nextDouble() * chanceMap.lastKey();
-            newMaterial = chanceMap.ceilingEntry(rand).getValue();
+            return chanceMap.ceilingEntry(rand).getValue();
         }
-
-        // ask config if physics should be used
-        block.setType(newMaterial, this.addon.getSettings().usePhysics());
-
-        return true;
     }
 
 
