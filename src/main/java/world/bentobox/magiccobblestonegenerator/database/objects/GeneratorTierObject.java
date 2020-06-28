d427cd593bc118443f06b42169e816a63e1bd789
BONNe <bonne@bonne.id.lv>
2020-06-28 13:59:20 +0300

    Add Getters and Setters for all variables.

--- src/main/java/world/bentobox/magiccobblestonegenerator/database/objects/GeneratorTierObject.java
+++ src/main/java/world/bentobox/magiccobblestonegenerator/database/objects/GeneratorTierObject.java
@@ -59,6 +59,248 @@ public class GeneratorTierObject implements DataObject
 	}
 
 
+	/**
+	 * Method GeneratorTierObject#getFriendlyName returns the friendlyName of this object.
+	 *
+	 * @return the friendlyName (type String) of this object.
+	 */
+	public String getFriendlyName()
+	{
+		return friendlyName;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setFriendlyName sets new value for the friendlyName of this object.
+	 * @param friendlyName new value for this object.
+	 *
+	 */
+	public void setFriendlyName(String friendlyName)
+	{
+		this.friendlyName = friendlyName;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getGeneratorIcon returns the generatorIcon of this object.
+	 *
+	 * @return the generatorIcon (type ItemStack) of this object.
+	 */
+	public ItemStack getGeneratorIcon()
+	{
+		return generatorIcon;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setGeneratorIcon sets new value for the generatorIcon of this object.
+	 * @param generatorIcon new value for this object.
+	 *
+	 */
+	public void setGeneratorIcon(ItemStack generatorIcon)
+	{
+		this.generatorIcon = generatorIcon;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getGeneratorType returns the generatorType of this object.
+	 *
+	 * @return the generatorType (type GeneratorType) of this object.
+	 */
+	public GeneratorType getGeneratorType()
+	{
+		return generatorType;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setGeneratorType sets new value for the generatorType of this object.
+	 * @param generatorType new value for this object.
+	 *
+	 */
+	public void setGeneratorType(GeneratorType generatorType)
+	{
+		this.generatorType = generatorType;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getRequiredMinIslandLevel returns the requiredMinIslandLevel of this object.
+	 *
+	 * @return the requiredMinIslandLevel (type long) of this object.
+	 */
+	public long getRequiredMinIslandLevel()
+	{
+		return requiredMinIslandLevel;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setRequiredMinIslandLevel sets new value for the requiredMinIslandLevel of this object.
+	 * @param requiredMinIslandLevel new value for this object.
+	 *
+	 */
+	public void setRequiredMinIslandLevel(long requiredMinIslandLevel)
+	{
+		this.requiredMinIslandLevel = requiredMinIslandLevel;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getRequiredBiomes returns the requiredBiomes of this object.
+	 *
+	 * @return the requiredBiomes (type Set<Biome>) of this object.
+	 */
+	public Set<Biome> getRequiredBiomes()
+	{
+		return requiredBiomes;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setRequiredBiomes sets new value for the requiredBiomes of this object.
+	 * @param requiredBiomes new value for this object.
+	 *
+	 */
+	public void setRequiredBiomes(Set<Biome> requiredBiomes)
+	{
+		this.requiredBiomes = requiredBiomes;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getRequiredPermissions returns the requiredPermissions of this object.
+	 *
+	 * @return the requiredPermissions (type Set<String>) of this object.
+	 */
+	public Set<String> getRequiredPermissions()
+	{
+		return requiredPermissions;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setRequiredPermissions sets new value for the requiredPermissions of this object.
+	 * @param requiredPermissions new value for this object.
+	 *
+	 */
+	public void setRequiredPermissions(Set<String> requiredPermissions)
+	{
+		this.requiredPermissions = requiredPermissions;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getGeneratorTierCost returns the generatorTierCost of this object.
+	 *
+	 * @return the generatorTierCost (type double) of this object.
+	 */
+	public double getGeneratorTierCost()
+	{
+		return generatorTierCost;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setGeneratorTierCost sets new value for the generatorTierCost of this object.
+	 * @param generatorTierCost new value for this object.
+	 *
+	 */
+	public void setGeneratorTierCost(double generatorTierCost)
+	{
+		this.generatorTierCost = generatorTierCost;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#isDeployed returns the deployed of this object.
+	 *
+	 * @return the deployed (type boolean) of this object.
+	 */
+	public boolean isDeployed()
+	{
+		return deployed;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setDeployed sets new value for the deployed of this object.
+	 * @param deployed new value for this object.
+	 *
+	 */
+	public void setDeployed(boolean deployed)
+	{
+		this.deployed = deployed;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getBlockChanceMap returns the blockChanceMap of this object.
+	 *
+	 * @return the blockChanceMap (type Map<Double, Material>) of this object.
+	 */
+	public Map<Double, Material> getBlockChanceMap()
+	{
+		return blockChanceMap;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setBlockChanceMap sets new value for the blockChanceMap of this object.
+	 * @param blockChanceMap new value for this object.
+	 *
+	 */
+	public void setBlockChanceMap(Map<Double, Material> blockChanceMap)
+	{
+		this.blockChanceMap = blockChanceMap;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getTreasureChanceMap returns the treasureChanceMap of this object.
+	 *
+	 * @return the treasureChanceMap (type Map<Double, Material>) of this object.
+	 */
+	public Map<Double, Material> getTreasureChanceMap()
+	{
+		return treasureChanceMap;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setTreasureChanceMap sets new value for the treasureChanceMap of this object.
+	 * @param treasureChanceMap new value for this object.
+	 *
+	 */
+	public void setTreasureChanceMap(Map<Double, Material> treasureChanceMap)
+	{
+		this.treasureChanceMap = treasureChanceMap;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#getTreasureChance returns the treasureChance of this object.
+	 *
+	 * @return the treasureChance (type double) of this object.
+	 */
+	public double getTreasureChance()
+	{
+		return treasureChance;
+	}
+
+
+	/**
+	 * Method GeneratorTierObject#setTreasureChance sets new value for the treasureChance of this object.
+	 * @param treasureChance new value for this object.
+	 *
+	 */
+	public void setTreasureChance(double treasureChance)
+	{
+		this.treasureChance = treasureChance;
+	}
+
+
 // ---------------------------------------------------------------------
 // Section: Enums
 // ---------------------------------------------------------------------
