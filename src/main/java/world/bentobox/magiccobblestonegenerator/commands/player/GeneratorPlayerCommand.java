5d149cc62515482ee2dac6522dd68e8ca1dfaa6d
BONNe <bonne@bonne.id.lv>
2020-12-03 15:58:05 +0200

    Add generator activate command for players.

--- src/main/java/world/bentobox/magiccobblestonegenerator/commands/player/GeneratorPlayerCommand.java
+++ src/main/java/world/bentobox/magiccobblestonegenerator/commands/player/GeneratorPlayerCommand.java
@@ -57,6 +57,7 @@ public class GeneratorPlayerCommand extends CompositeCommand
 
 		new GeneratorViewPlayerCommand(this);
 		new GeneratorBuyPlayerCommand(this);
+		new GeneratorActivatePlayerCommand(this);
 
 		this.setOnlyPlayer(true);
 	}
@@ -214,7 +215,7 @@ public class GeneratorPlayerCommand extends CompositeCommand
 
 
 	/**
-	 * This class allows to open GUI for viewing generator directly.
+	 * This class allows to purchase generator directly.
 	 */
 	private static class GeneratorBuyPlayerCommand extends CompositeCommand
 	{
@@ -298,6 +299,128 @@ public class GeneratorPlayerCommand extends CompositeCommand
 					addonManager.purchaseGenerator(user, data, generator);
 					return true;
 				}
+			}
+			else
+			{
+				this.showHelp(this, user);
+			}
+
+			return false;
+		}
+
+
+		/**
+		 * This method adds tab complete for view command.
+		 * @param user the {@link User} who is executing this command.
+		 * @param alias the label which has been used to execute this command. It can be
+		 * {@link CompositeCommand#getLabel()} or an alias.
+		 * @param args the command arguments.
+		 * @return List with possible generator tier Id's
+		 */
+		@Override
+		public Optional<List<String>> tabComplete(User user, String alias, List<String> args)
+		{
+			return GeneratorPlayerCommand.generatorsTabComplete(this.getAddon(), user, this.getWorld(), args);
+		}
+	}
+
+
+	/**
+	 * This class allows to activate/deactivate generator directly.
+	 */
+	private static class GeneratorActivatePlayerCommand extends CompositeCommand
+	{
+		/**
+		 * This is simple constructor for initializing /{player_command} generator command.
+		 * @param parentCommand Parent Command where we hook our command into.
+		 */
+		public GeneratorActivatePlayerCommand(CompositeCommand parentCommand)
+		{
+			super(parentCommand.getAddon(), parentCommand, "activate");
+		}
+
+
+		/**
+		 * Setups anything that is needed for this command. <br/><br/> It is recommended you
+		 * do the following in this method:
+		 * <ul>
+		 * <li>Register any of the sub-commands of this command;</li>
+		 * <li>Define the permission required to use this command using {@link
+		 * CompositeCommand#setPermission(String)};</li>
+		 * <li>Define whether this command can only be run by players or not using {@link
+		 * CompositeCommand#setOnlyPlayer(boolean)};</li>
+		 * </ul>
+		 */
+		@Override
+		public void setup()
+		{
+			this.inheritPermission();
+			this.setParametersHelp(Constants.PLAYER_COMMANDS + "activate.parameters");
+			this.setDescription(Constants.PLAYER_COMMANDS + "activate.description");
+
+			this.setOnlyPlayer(true);
+		}
+
+
+		/**
+		 * Defines what will be executed when this command is run.
+		 *
+		 * @param user the {@link User} who is executing this command.
+		 * @param label the label which has been used to execute this command. It can be
+		 * {@link CompositeCommand#getLabel()} or an alias.
+		 * @param args the command arguments.
+		 * @return {@code true} if the command executed successfully, {@code false} otherwise.
+		 */
+		@Override
+		public boolean execute(User user, String label, List<String> args)
+		{
+			if (args.size() == 1 || args.size() == 2)
+			{
+				StoneGeneratorManager addonManager = this.<StoneGeneratorAddon>getAddon().getAddonManager();
+				GeneratorTierObject generator = addonManager.getGeneratorByID(args.get(0));
+
+				if (generator == null)
+				{
+					Utils.sendMessage(user,
+						user.getTranslation(Constants.ERRORS + "generator-tier-not-found",
+							Constants.GENERATOR, args.get(0),
+							Constants.GAMEMODE, Utils.getGameMode(this.getWorld())));
+					return false;
+				}
+
+				Island island = this.getAddon().getIslands().getIsland(this.getWorld(), user);
+
+				if (island == null)
+				{
+					Utils.sendMessage(user, user.getTranslation("general.errors.no-island"));
+					return false;
+				}
+
+				GeneratorDataObject data = addonManager.getGeneratorData(island);
+
+				if (data == null)
+				{
+					Utils.sendMessage(user, user.getTranslation("general.errors.no-island"));
+					return false;
+				}
+
+				boolean activate = args.size() != 2 || Boolean.parseBoolean(args.get(1));
+
+				if (activate)
+				{
+					// Check before activating.
+					if (addonManager.canActivateGenerator(user, data, generator))
+					{
+						addonManager.activateGenerator(user, island, data, generator);
+					}
+				}
+				else
+				{
+					// always can remove from set.
+					addonManager.deactivateGenerator(user, data, generator);
+				}
+
+				return true;
 			}
 			else
 			{
