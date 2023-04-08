5d082771db355238764f819f0ac882bc6d0ff914
BONNe <bonne@bonne.id.lv>
2023-04-08 18:33:56 +0300

    Create plugin.yml (#126)
    * Create plugin.yml
    
    * Update pom.xml
    
    * Update StoneGeneratorPladdon.java

--- src/main/java/world/bentobox/magiccobblestonegenerator/StoneGeneratorPladdon.java
+++ src/main/java/world/bentobox/magiccobblestonegenerator/StoneGeneratorPladdon.java
@@ -7,9 +7,6 @@
 package world.bentobox.magiccobblestonegenerator;
 
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
@@ -17,8 +14,6 @@ import world.bentobox.bentobox.api.addons.Pladdon;
 /**
  * @author tastybento
  */
-@Plugin(name="MagicCobblestoneGenerator", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_17)
 public class StoneGeneratorPladdon extends Pladdon
 {
     @Override
